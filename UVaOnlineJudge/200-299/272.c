18d5dce45f93defbab9edab3204008580d00085a
Li-Jyu Gao <b8989332@gmail.com>
2019-07-05 23:43:10 +0800

    first commit

---
+++ UVaOnlineJudge/200-299/272.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char c;
+    int flag = 1;
+    while((c = getchar()) != EOF)
+    {
+        if(c == '"'){
+            if(flag) {
+               printf("``");
+            }
+            else{
+                printf("''");
+            }
+            flag = !flag;
+        }
+        else{
+            printf("%c", c);
+        }
+    }
+
+
+    return 0;
+}
