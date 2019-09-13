e7dde44aaa823e6dbbfac7db16b16c52e38bc8a6
Li-Jyu Gao <b8989332@gmail.com>
2019-09-13 20:48:36 +0800

    update

--- UVaOnlineJudge/200-299/202.c
+++ UVaOnlineJudge/200-299/202.c
@@ -1,98 +1,67 @@
 #include <stdio.h>
-char cycle[35000];
-int pos[35000];
+int cycle[3005];
+int pos[3005];
 int main()
 {
     int i,j,m,n;
     int tempM, tempN;
-    int quotient, remainder, tempRemainder;
-    int isDigit, cycleLen, noCycle = 0;
+    int quotient, remainder;
+    int maxLen = 50;
+    int cycleLen;
     while(scanf("%d %d", &m, &n) == 2){
         tempM = m;
         tempN = n;
 
-        for(i = 0 ; i < n * 10;++i)
+        for(i = 0 ; i < n;++i)
         {
             pos[i] = -1;
         }
 
-        isDigit = 0;
         i = 0;
-        noCycle = 0;
-        quotient = m/n;
         remainder = m % n;
-        if(remainder != 0){
-            isDigit = 1;
-        }
-        else{
-            noCycle = 1;
+        quotient = m / n;
+        while(remainder && pos[remainder] == -1) {
+
+            pos[remainder] = i;
+            cycle[i++] = remainder * 10 / n;
+            remainder = (remainder * 10) % n;
         }
 
-        if(isDigit) {
-            while(1){
-                remainder *= 10;
-                if(pos[remainder] != -1){
-                    break;
-                }
-                if(remainder < n){
-                    cycle[i] = '0';
-                    pos[remainder] = i;
-                }
-                else{
-                    cycle[i] = (remainder / n) + '0';
-                    pos[remainder] = i;
-                    tempRemainder = remainder % n;
-                    if(tempRemainder == 0){
-                        noCycle = 1;
-                        break;
-                    }
-                    remainder = tempRemainder;
-                }
-                i++;
+
+        printf("%d/%d = %d.", m, n, quotient);
+
+        if(remainder) {
+            for(j = 0 ; j < pos[remainder];++j)
+            {
+                printf("%d", cycle[j]);
             }
 
-            if(i >= 50){
-                cycle[50] = '\0';
+            printf("(");
+            for(;j < i && j < maxLen;++j)
+            {
+                printf("%d", cycle[j]);
             }
-            else{
-                cycle[i+1] = '\0';
-            }
-        }
 
-        if(noCycle){
-            cycleLen = 1;
-        }
-        else{
-            cycleLen = i - pos[remainder];
-        }
-
-        printf("%d/%d = %d", m, n, quotient);
-        if(isDigit){
-            printf(".");
-            if(noCycle){
-                printf("%s(0)\n", cycle);
+            if(j == i)
+            {
+                printf(")\n");
             }
-            else{
-                for(j = 0;j < (i >= 50 ? 50 : i);++j)
-                {
-                    if(j == pos[remainder])
-                    {
-                        printf("(");
-                    }
-                    printf("%c", cycle[j]);
-                }
-                if(i >= 50){
-                    printf("...)\n");
-                }
-                else{
-                    printf(")\n");
-                }
+            else
+            {
+                printf("...)\n");
             }
         }
         else{
-            printf(".(0)\n");
+            for(j = 0 ; j < i;++j){
+                printf("%d", cycle[j]);
+            }
+            printf("(0)\n");
         }
+
+        cycleLen = remainder ? i - pos[remainder] : 1;
         printf("   %d = number of digits in repeating cycle\n\n", cycleLen);
+
+
 
     }
     return 0;
