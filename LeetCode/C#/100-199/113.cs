70167ed0172d97afbf40d6dd283ea82af5f7cafd
lijyugao <b8989332@gmail.com>
2020-09-20 00:04:23 +0800

    modify full example in LeetCode 113

--- LeetCode/C#/100-199/113.cs
+++ LeetCode/C#/100-199/113.cs
@@ -1,33 +1,90 @@
-public class Solution {
-    public IList<IList<int>> PathSum(TreeNode root, int sum) {
-        IList<IList<int>> result = new List<IList<int>>();
-        if(root == null){
+using System;
+using System.Collections.Generic;
+
+namespace LeetCode113
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public class Solution
+    {
+        public IList<IList<int>> PathSum(TreeNode root, int sum)
+        {
+            IList<IList<int>> result = new List<IList<int>>();
+            if (root == null)
+            {
+                return result;
+            }
+
+            IList<int> curNodes = new List<int>();
+            PathSum(root, sum, 0, curNodes, result);
             return result;
         }
-        
-        IList<int> curNodes = new List<int>();
-        PathSum(root, sum, 0, curNodes, result);
-        return result;
+
+        private void PathSum(TreeNode curNode, int targetSum, int sum, IList<int> curNodes, IList<IList<int>> result)
+        {
+            sum += curNode.val;
+            curNodes.Add(curNode.val);
+            if (curNode.left == null && curNode.right == null && sum == targetSum)
+            {
+                var copyList = new List<int>();
+                copyList.AddRange(curNodes);
+                result.Add(copyList);
+                return;
+            }
+
+            if (curNode.left != null)
+            {
+                PathSum(curNode.left, targetSum, sum, curNodes, result);
+                curNodes.RemoveAt(curNodes.Count - 1);
+            }
+
+            if (curNode.right != null)
+            {
+                PathSum(curNode.right, targetSum, sum, curNodes, result);
+                curNodes.RemoveAt(curNodes.Count - 1);
+            }
+        }
     }
-    
-    private void PathSum(TreeNode curNode, int targetSum, int sum, IList<int> curNodes, IList<IList<int>> result){
-        sum += curNode.val;
-        curNodes.Add(curNode.val);
-        if(curNode.left == null && curNode.right == null && sum == targetSum){
-            var copyList = new List<int>();
-            copyList.AddRange(curNodes);
-            result.Add(copyList);
-            return;
-        }
-        
-        if(curNode.left != null){
-            PathSum(curNode.left, targetSum, sum, curNodes, result);
-            curNodes.RemoveAt(curNodes.Count - 1);
-        }
-        
-        if(curNode.right != null){
-            PathSum(curNode.right, targetSum, sum, curNodes, result);
-            curNodes.RemoveAt(curNodes.Count - 1);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TreeNode node7 = new TreeNode(7, null, null);
+            TreeNode node8 = new TreeNode(2, null, null);
+            TreeNode node4 = new TreeNode(11, node7, node8);
+            
+            TreeNode node2 = new TreeNode(4, node4, null);
+            
+            TreeNode node9 = new TreeNode(5, null, null);
+            TreeNode node10 = new TreeNode(1, null, null);
+            
+            TreeNode node6 = new TreeNode(4, node9, node10);
+
+            TreeNode node5 = new TreeNode(13, null, null);
+            
+            TreeNode node3 = new TreeNode(8, node5, node6);
+            TreeNode node1 = new TreeNode(5, node2, node3);
+            
+            Solution sol = new Solution();
+            IList<IList<int>> result = sol.PathSum(node1, 22);
+            for(int i = 0 ; i < result.Count;++i){
+                for(int j = 0;j < result[i].Count;++j){
+                    Console.Write(" " + result[i][j]);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
         }
     }
 }
--- LeetCode/C++/100-199/113.cpp
+++ LeetCode/C++/100-199/113.cpp
@@ -1,3 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// Definition for a Node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
 class Solution {
 public:
     vector<vector<int>> pathSum(TreeNode* root, int sum) {
@@ -30,3 +44,33 @@ private:
         }
     }
 };
+
+int main()
+{
+	TreeNode node7(7, NULL, NULL);
+	TreeNode node8(2, NULL, NULL);
+	TreeNode node4(11, &node7, &node8);
+	
+	TreeNode node2(4, &node4, NULL);
+	
+	TreeNode node9(5, NULL, NULL);
+	TreeNode node10(1, NULL, NULL);
+	
+	TreeNode node6(4, &node9, &node10);
+
+	TreeNode node5(13, NULL, NULL);
+	
+    TreeNode node3(8, &node5, &node6);
+    TreeNode node1(5, &node2, &node3);
+    
+    Solution sol;
+    vector<vector<int>> result = sol.pathSum(&node1, 22);
+	for(int i = 0 ; i < result.size();++i){
+		for(int j = 0;j < result[i].size();++j){
+			cout << " " << result[i][j];
+		}
+		cout << endl;
+	}
+
+    return 0;
+}
